11d1905afa4152af93e1fc7999171860bc17a59f
yanelisserrano <yaneliserranotaylor@gmail.com>
2019-10-31 09:00:39 +0100

    Tienda Manejo de Obbjetos

--- Tienda/src/tienda/Main.java
+++ Tienda/src/tienda/Main.java
@@ -7,6 +7,31 @@ public class Main {
 	static Tienda td  = new Tienda(); 
 	
 	public static void main(String[] args) {
+		
+		int opcion = -1; 
+		do {
+			 System.out.println("1)Insertar, 2)AddStock, 3) Mostrar Catalogo -1)Salir");
+			 opcion = sc.nextInt(); 
+			 
+			 switch (opcion) {
+			case 1:
+				insertarProductos();
+				break;
+			case 2:
+				añadirStock();
+				break;
+			case 3:
+				mostrarCatalogo();
+				break;
+			default:
+				break;
+			}
+			
+		} while (opcion !=-1 );
+
+	}
+	
+	public static void insertarProductos() {
 		System.out.println("Dime el nombre del producto");
 		String nombre = sc.next(); 
 
@@ -20,9 +45,42 @@ public class Main {
 		int codigo = sc.nextInt(); 
 		
 		td.addProducto(nombre, precio, tipo, codigo);
-		
-		
 
 	}
 
+	public static void añadirStock() {
+		System.out.println("Dime el codigo del producto");
+		int codigo = sc.nextInt(); 
+		
+		System.out.println("Dime la cantidad de kilogramos del producto");
+		float kilogramos = sc.nextFloat();
+		
+		int respuesta = td.addStock(codigo, kilogramos); 
+		if(respuesta == 0) {
+			System.out.println("Stock Insertado correcatamnete");
+		}
+		if(respuesta == -2) {
+			System.out.println("Producto No encontrado");
+		}
+		if(respuesta == -3) {
+			System.out.println("Los kilogramos no pueden ser negativos");
+		}
+	}
+	
+	public static void mostrarCatalogo() {
+		
+		Producto lista[]= td.mostrarCatalogo();
+		
+		for (int i = 0; i < lista.length; i++) {
+		
+			
+			if(lista[i]!=null) {
+			System.out.println(lista[i].getCodigo() + " - "+ lista[i].getNombre());
+			System.out.println(lista[i].getTipo());
+			System.out.println("Stock : "+ lista[i].getStock());
+			System.out.println("Precio : "+ lista[i].getPrecio() + " €/kg");
+			}
+		}
+	}
+	
 }
--- Tienda/src/tienda/Tienda.java
+++ Tienda/src/tienda/Tienda.java
@@ -46,7 +46,30 @@ public class Tienda {
 		
 	}
 	
+	public int addStock(int codigo, float kilogramos){
+		
+		if(kilogramos <=0) {
+			return -3; 
+		}
+		boolean encontrado =false; 
+		//Primero buscar el producto a traves del codigo
+		for (int i = 0; i < listadoproductos.length; i++) {
+			if(listadoproductos[i]!=null) {
+			if(listadoproductos[i].getCodigo()== codigo) {
+				listadoproductos[i].setStock(listadoproductos[i].getStock()+kilogramos);
+				encontrado = true; 
+				return 0; 
+			}
+			}
+		}
+		if(encontrado == false) {
+			return -2; 
+		}
+		return 0; 
+	}
 	
+	public Producto[] mostrarCatalogo() {
+		return listadoproductos; 
+	}
 	
-
 }
