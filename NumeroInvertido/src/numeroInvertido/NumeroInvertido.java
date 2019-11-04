0cfc5972d5a33c14026e20026d36ad225cabfcbc
yanelisserrano <yaneliserranotaylor@gmail.com>
2019-11-04 09:24:30 +0100

    Numero Invertido

--- NumeroInvertido/src/numeroInvertido/NumeroInvertido.java
+++ NumeroInvertido/src/numeroInvertido/NumeroInvertido.java
@@ -1,11 +1,13 @@
-package numeroCifras;
+package numeroInvertido;
 
 import java.util.Scanner;
 
 /*
- * Pide un número entre 0 y 99999, e indica cuántas cifras tiene.
+ * Pide un número entre 0 y 99999 y mostrar las cifras al revés.
  * */
-public class NumeroCifras {
+
+public class NumeroInvertido {
+	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
@@ -16,30 +18,38 @@ public class NumeroCifras {
 		System.out.println("Dime un numero entre  1 y 99999");
 		 numero = sc.next(); 
 		if(numero.length()>1 && numero.length()<5) {
-		System.out.println("La longitud del numero es "+numero.length());
+		 String numeros[] = numero.split(""); 
+		 String numeroInvertido = "";
+		 for (int i = 0; i < numeros.length; i++) {
+			numeroInvertido = numeroInvertido+""+numeros[i];
+		}
+		 System.out.println("El numero invertido es "+ numeroInvertido);
 		}
 		else {
 			System.out.println("El numero introducido está fuera del rango especificado");
 		}
 		}while(numero.length()<1 || numero.length()>5);
 		
-		//Solucion con formato Numerico
-        int n, cifras;
+		
+		//Solución con formato Numerico
+        int n, cifrasInvertidas, resto;
         String car;
         do{
             System.out.print("Introduce un número entero: ");
             n = sc.nextInt();
-            cifras= 0;    //esta variable es el contador de cifras
+            cifrasInvertidas= 0;    //esta variable será el numero invertido
             while(n!=0){             //mientras a n le queden cifras
-                    n = n/10;         //le quitamos el último dígito
-                   cifras++;          //sumamos 1 al contador de cifras
+                    resto = n%10;
+                    cifrasInvertidas = cifrasInvertidas* 10+ resto;
+                    n =n/10; 
             }
-            System.out.println("El número tiene " + cifras+ " cifras");
+            System.out.println("El número invertido es " + cifrasInvertidas);
             System.out.print("Continuar? ");
             car = sc.next();
         }while(car!="n" && car != "N");   
 		
 		
 	}
+
 
 }
--- NumeroMultiplos/src/numeroMultiplos/NumerosMultiplos.java
+++ NumeroMultiplos/src/numeroMultiplos/NumerosMultiplos.java
@@ -1,3 +1,4 @@
+
 package numeroMultiplos;
 
 import java.util.Scanner;
